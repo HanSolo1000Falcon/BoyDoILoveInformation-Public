eaf20d390aee5c0395fc256cdb3af9933f642b02
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2025-12-24 20:18:12 +0000

    i meant ping checks not fps checks :sob:

--- BoyDoILoveInformation/Constants.cs
+++ BoyDoILoveInformation/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string PluginGuid    = "hansolo1000falcon.boydoiloveinformation";
     public const string PluginName    = "BoyDoILoveInformation";
-    public const string PluginVersion = "1.0.0";
+    public const string PluginVersion = "1.0.1";
 }
