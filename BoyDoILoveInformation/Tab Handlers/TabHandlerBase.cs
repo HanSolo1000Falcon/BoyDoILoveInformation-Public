3cd0fb1c4d4689dc54f55cc41666b426d7a2b3db
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-12-31 23:37:34 +0100

    added a comment for everyone reading small parts of the code or people that are simply unintelligent

--- BoyDoILoveInformation/Tab Handlers/TabHandlerBase.cs
+++ BoyDoILoveInformation/Tab Handlers/TabHandlerBase.cs
@@ -2,4 +2,6 @@ using UnityEngine;
 
 namespace BoyDoILoveInformation.Tab_Handlers;
 
-public class TabHandlerBase : MonoBehaviour { } // ts is as empty as can be twin
+// fyi for anyone reading the code and wondering why this class is empty:
+// this class is empty because it is solely meant to be inherited from so that i can grab all tabhandlers at MenuHandler:92 (line might not be exactly correct, cba to change every time i update menuhandler)
+public class TabHandlerBase : MonoBehaviour { }
