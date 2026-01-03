5b1fb43fb8b73c5afe54e702eeeaa1d52af3e14e
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-01-03 19:11:03 +0100

    remove console

--- BoyDoILoveInformation/Plugin.cs
+++ BoyDoILoveInformation/Plugin.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
-using BoyDoILoveInformation.Console;
 using BoyDoILoveInformation.Core;
 using BoyDoILoveInformation.Tools;
 using ExitGames.Client.Photon;
@@ -131,37 +129,6 @@ public class Plugin : BaseUnityPlugin
         gameObject.AddComponent<PunCallbacks>();
         gameObject.AddComponent<NetworkingHandler>();
         gameObject.AddComponent<MenuHandler>();
-        
-        string ConsoleGUID = "goldentrophy_Console"; // Do not change this, it's used to get other instances of Console
-        GameObject ConsoleObject = GameObject.Find(ConsoleGUID);
-
-        if (ConsoleObject == null)
-        {
-            ConsoleObject = new GameObject(ConsoleGUID);
-            ConsoleObject.AddComponent<Console.Console>();
-        }
-        else
-        {
-            if (ConsoleObject.GetComponents<Component>()
-                             .Select(c => c.GetType().GetField("ConsoleVersion",
-                                             BindingFlags.Public |
-                                             BindingFlags.Static |
-                                             BindingFlags.FlattenHierarchy))
-                             .Where(f => f != null && f.IsLiteral && !f.IsInitOnly)
-                             .Select(f => f.GetValue(null))
-                             .FirstOrDefault() is string consoleVersion)
-            {
-                if (ServerData.VersionToNumber(consoleVersion) < ServerData.VersionToNumber(Console.Console.ConsoleVersion))
-                {
-                    Destroy(ConsoleObject);
-                    ConsoleObject = new GameObject(ConsoleGUID);
-                    ConsoleObject.AddComponent<Console.Console>();
-                }
-            }
-        }
-
-        if (ServerData.ServerDataEnabled)
-            ConsoleObject.AddComponent<ServerData>();
     }
 
     private void FetchModsAndCheatsAndPlayerIDs()
